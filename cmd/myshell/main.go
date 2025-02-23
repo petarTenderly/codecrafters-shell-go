8c0112ad762f9e3f9e6884a8bd7ccdcbd514487a
Petar Drakulic <petar@tenderly.co>
2025-02-23 20:16:24 +0100

    cH

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,15 +10,24 @@ import (
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Fprint
 
+const (
+	ExitCmd = "exit 0"
+)
+
 func main() {
 	// Uncomment this block to pass the first stage
 	for true {
 		fmt.Fprint(os.Stdout, "$ ")
 
 		// Wait for user input
-		cmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		rawCmd, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		cmd := strings.TrimSpace(rawCmd)
+		switch cmd {
+		case ExitCmd:
+			os.Exit(0)
+		}
 
-		fmt.Printf("%s: command not found\n", strings.TrimSpace(cmd))
+		fmt.Printf("%s: command not found\n", cmd)
 	}
 
 }
